f80c1b0c6306a3849a99b64adb2f2c8e7ab9a4b3
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2016-11-12 23:32:33 +0200

    almost done html

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+    return gulp.src('./sass/**/*.sass')
+        .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+    gulp.watch('./sass/**/*.sass', ['sass']);
 });
+
+gulp.task('default', ['watch']);
